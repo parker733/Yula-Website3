2a67d3cf60a3f3bc6aa06d8d587ec8cf74ef9588
Parker <parker@itsparkerlee.com>
2019-02-08 12:45:20 -0500

    update background

---
+++ js/logic2.js
@@ -0,0 +1,13 @@
+	console.log(window.location.href);
+	if (window.location.href.indexOf("Craigslist") > -1) {
+	    $('body').css('background-color', '#0e007f');
+    }
+    else if (window.location.href.indexOf("fire_tongue") > -1) {
+	    $('body').css('background-color', '#000000');
+    }
+   	else if (window.location.href.indexOf("xiangqi") > -1) {
+	    $('body').css('background-color', '#111111');
+    }
+   	else if (window.location.href.indexOf("book") > -1) {
+	    $('body').css('background-color', '#000000');
+    }
